cec07f4c3b6c76896092905e497437ac614d5f6f
Juan Muniz Rivera <43590739+JSkyBound@users.noreply.github.com>
2025-01-10 10:55:52 -0400

    Initial commit

---
+++ Source/GAM312_Muniz/GAM312_Muniz.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_Muniz.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_Muniz, "GAM312_Muniz" );
